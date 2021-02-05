3e10dfdb9a387d3af41ed6bd38fbf1e8d18804b5
Lilian Kasem <likasem@microsoft.com>
2021-02-04 20:19:58 -0800

    Update script to include Azure Functions

--- .pipelines/scripts/Generate-Connections.ps1
+++ .pipelines/scripts/Generate-Connections.ps1
@@ -8,20 +8,30 @@
     .PARAMETER outputLocation
     The path to store the updated connections json file. Defaults to connections.json in the local directory.
 
+    .PARAMETER withFunctions
+    A flag to include Azure Functions in the connection file.
+
     .EXAMPLE
         Generates a connections json file.
 
         ./Generate-Connections.ps1 -resourceGroup rg-api-connections -outputLocation connections.json
+
+        Use this if you want to include function connectors
+        ./Generate-Connections.ps1 -resourceGroup rg-api-connections -outputLocation connections.json -withFunctions
 #>
 
 param(
-    [Parameter(Mandatory = $True)]
-    [string]
-    $resourceGroup = "",
+  [Parameter(Mandatory = $True)]
+  [string]
+  $resourceGroup,
 
-    [Parameter(Mandatory = $False)]
-    [string]
-    $outputLocation = "connections.json"
+  [Parameter(Mandatory = $False)]
+  [string]
+  $outputLocation = "connections.json",
+
+  [Parameter(Mandatory = $False)]
+  [switch]
+  $withFunctions
 )
 
 Function Get-ConnectionsFile {
@@ -32,33 +42,85 @@ Function Get-ConnectionsFile {
   Write-Host 'Looking up API Connectors'
   $apiConnections = (Get-ApiConnections) ?? @{}
 
-  $json = @{ "managedApiConnection" = $apiConnections; } | ConvertTo-Json -Depth 10 -Compress
+  if ($withFunctions) {
+    Write-Host 'Looking up Function Connectors'
+    $functionConnections = (Get-FunctionConnections) ?? @{}
+  }
+
+  $json = $withFunctions ? @{ "managedApiConnection" = $apiConnections; "functionConnections" = $functionConnections } : @{ "managedApiConnection" = $apiConnections; }
+  $json = ConvertTo-Json $json -Depth 5 -Compress
   $json = [Regex]::Replace($json, "\\u[a-zA-Z0-9]{4}", { param($u) [Regex]::Unescape($u) })
   $json | Set-Content -Path $outputLocation
 }
 
 Function Get-ApiConnections {
+  $apiConnections = @{}
   $resources = Get-AzResource -ResourceGroupName $resourceGroup -ResourceType Microsoft.Web/connections
-  return @(
-    $resources | ForEach-Object {
-      Write-Host 'Found API connector: '$_.Name
-      $connectionResource = Get-AzResource -ResourceId $_.id
-      return @{
-        $_.Name = @{
-          "api"                  = @{
-            "id" = $connectionResource.Properties.api.id
-          };
-          "connection"           = @{
-            "id" = $_.Id.ToLower();
-          };
-          "connectionRuntimeUrl" = $connectionResource.Properties.connectionRuntimeUrl;
-          "authentication"       = @{
-            "type" = "ManagedServiceIdentity"
+  $resources | ForEach-Object {
+    $name = $_.Name;
+    Write-Host 'Found API connector: '$name
+    $connectionResource = Get-AzResource -ResourceId $_.id
+    $apiConnections["$name"] = @{
+      "api"                  = @{
+        "id" = $connectionResource.Properties.api.id
+      };
+      "connection"           = @{
+        "id" = $_.Id.ToLower();
+      };
+      "connectionRuntimeUrl" = $connectionResource.Properties.connectionRuntimeUrl;
+      "authentication"       = @{
+        "type" = "ManagedServiceIdentity"
+      }
+    }
+  }
+  return $apiConnections;
+}
+
+Function Get-FunctionConnections {
+  $functionConnections = @{}
+  $resources = Get-AzResource -ResourceGroupName $resourceGroup -ResourceType Microsoft.Web/sites
+  $resources | ForEach-Object {
+    Write-Host 'Found function app: '$_.Name
+
+    $resourceId = $_.id
+    $function = func azure functionapp list-functions $_.Name --show-keys
+
+    if ($function -match "(Invoke url)") {
+      $function | select-string -Pattern "Invoke url:" -SimpleMatch | ForEach-Object {
+
+        $line = $_.Line -replace '\s', ''
+        $name = [Regex]::Matches($line, "(?<=api\/)(.*)(?=\?)")
+        $trigger = [Regex]::Matches($line, "(?<=url:)(.*)(?=\?)")
+        $key = [Regex]::Matches($line, "(?<=code=).*$")
+
+        Write-Host "Found function: $name"
+
+        $functionResource = Get-AzResource -ResourceId ($resourceId + "/functions/$name")
+        if ($functionResource -and $functionResource.ResourceType -eq "Microsoft.Web/sites/functions") {
+          $functionAuth = @{
+            "type"  = "QueryString";
+            "name"  = "Code";
+            "value" = "$key";
           }
+          $functionConnections["$name"] = @{
+            "function"       = @{
+              "id" = $functionResource.ResourceId
+            };
+            "triggerUrl"     = "$trigger"
+            "authentication" = $functionAuth
+            "displayName"    = "$name"
+          }
+        }
+        else {
+          Write-Host "Issue adding function $name"
         }
       }
     }
-  )
+    else {
+      Write-Host $_.Name 'does not contain any functions, or might be a logic app.'
+    }
+  }
+  return $functionConnections;
 }
 
 #******************************************************************************
